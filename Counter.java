31419873074def55da2f0dd0644ee78026619ea5
fcorpus <fcastillocorpus@gmail.com>
2023-05-30 09:14:20 -0600

    LeaderBoard v1

--- Counter.java
+++ Counter.java
@@ -12,14 +12,18 @@ public class Counter extends Actor
      * Act - do whatever the Counter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int score=0;
+    public static int score=0;
     int money=0;
     public Counter(){
         setImage(new GreenfootImage("Score: "+score+"\n Cash: "+money,40,Color.BLACK,new Color(0,0,0,0)));
+        
     }
     public void act()
     {
         setImage(new GreenfootImage("Score: "+score+"\n Cash: "+money,40,Color.BLACK,new Color(0,0,0,0)));
         // Add your action code here.
+    }
+    public static int getScore(){
+        return score;
     }
 }
