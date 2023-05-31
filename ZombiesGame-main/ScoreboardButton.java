267ca3b3703ba19c18e1b5e18b6dd5c1913b1b26
Santiago Luna Mireles <a335833@alumnos.uaslp.mx>
2023-05-31 11:54:50 -0600

    chequeo

---
+++ ZombiesGame-main/ScoreboardButton.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ScoreboardButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ScoreboardButton extends Button
+{
+    /**
+     * Act - do whatever the ScoreboardButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        super.act(this.getClass().getName());
+    }
+    
+    public void onButtonClicked(){
+        Greenfoot.setWorld(new ScoreboardScreen());
+    }
+}
